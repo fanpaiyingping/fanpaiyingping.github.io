1dbbf53ad57861298feb3d45babfb2e49a3bdd7f
fanpaiyingping <fanpaiyingping@gmail.com>
2017-12-07 00:57:05 -0500

    Site updated: 2017-12-07 00:57:05

--- js/theme.js
+++ js/theme.js
@@ -20,12 +20,11 @@ function changeLayoutOnTouchScreen() {
 			el.classList.add("Card-excerpt-notouch")
 		});
 		// set to hide (tags) in cards, as it is involved in exerpt already
-		/*
-		elems = document.querySelectorAll(".Card-tags");
+		
+		elems = document.querySelectorAll(".Card-excerpt-tags");
 		[].forEach.call(elems, function(el) {
 			el.style.display = "none";
 		});
-		*/
 
 		elems = document.querySelectorAll(".Card-meta");
 		[].forEach.call(elems, function(el) {
